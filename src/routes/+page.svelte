10d94a0172c5f9669650e309a6b15db3b144324b
Robert Delaney <Doggybootsy@gmail.com>
2026-01-22 21:14:19 -0800

    add back contact info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="wrap">
 	<p>Hey, I'm DoggyBootsy!</p>
-	<!-- <p>You can contact me at <Anchor href="mailto:contact@doggybootsy.com">contact@doggybootsy.com</Anchor></p> -->
+	<p>You can contact me at <Anchor href="mailto:contact@doggybootsy.com">contact@doggybootsy.com</Anchor></p>
 
 	<p>I am the lead developer for the <Anchor href="https://betterdiscord.app">BetterDiscord</Anchor> client</p>
 </div>
